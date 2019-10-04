56d299dc37b6f069481e86c7f36a8ae18a678b25
Amin-Urcid <36415464+Amin-Urcid@users.noreply.github.com>
2019-10-03 22:11:55 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,39 @@
+//import java.util.*;
+
+//import java.lang.*;
+
+//import java.io.*;
+
+import java.util.Scanner;
+
+
+
+// The main method must be in a class named "Main"
+
+
+
+class Main //Programa que ermite la entrada de datos
+
+{
+    
+	public static void main(String[] args)
+    
+	{
+        
+		Scanner entrada = new Scanner(System.in);
+ 
+		int numero;
+
+        
+        System.out.print("Teclee el numero:");
+
+	        numero = entrada.nextInt();
+  
+
+
+  
+        System.out.print("\nEl numero ingresado es:" +numero);
+
+    }
+
+}
